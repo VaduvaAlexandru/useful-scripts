b2959de2ac2f6044540a34749b07591e5f989914
Alexandru.Vaduva <vaduva.jan.alexandru@gmail.com>
2014-09-08 00:10:08 +0300

    Finished the implementation for arguments parsing on git script.
    Signed-off-by: Alexandru.Vaduva <vaduva.jan.alexandru@gmail.com>

--- git-commit.py
+++ git-commit.py
@@ -1,46 +1,109 @@
 #!/usr/bin/python
 
 import getopt, sys
+import re
+import os.path
+import git
+
+#Define software version
+version = 1.0
+
+#Define the emailing list
+email_list = []
+names_list = []
+
+#Define other global variables
+chechM = False
+checkC = False
+filename = "a"
 
 def inspect_args():
     try:
-        opts, args = getopt.getopt(sys.argv[1:], 'hi:o:vtbpms:', ['help', 'input=', 'output='])
+        opts, args = getopt.getopt(sys.argv[1:], 'hlvmcf:', ['help', 'list', 'verbose', 'maintainer', 'committers', 'filename='])
         if not opts:
-            print 'No options supplied..'
+            print "No options supplied. Default options enabled:"
+            print "\tReviewer and merger selected from the given list file."
+            read_list()
             usage()
     except getopt.GetoptError,e:
         # print help information and exit:
         print str(e)
         usage()
-        sys.exit(2)
+        sys.exit()
+
+    file_status = False
+    opts_check = False
+    global checkM
+    global checkC
+    global filename
     for opt, arg in opts:
         if opt in ('-h', '--help'):
+            opts_check = True
             usage()
-            sys.exit(2)
-    output = None
-    verbose = False
-#    for o, a in opts:
-#	if o == "-v":
-#            verbose = True
-#        elif o in ("-h", "--help"):
-#            usage()
-#            sys.exit()
-#        elif o in ("-o", "--output"):
-#            output = a
-#        else:
-#            assert False, "unhandled option"
+            sys.exit()
+        if opt in ('-v', '--version'):
+            opts_check = True
+            print  "Software version: " + str(version)
+            sys.exit()
+        if opt in ('-f', "--filename"):
+            opts_check = True
+            file_status = True
+            if os.path.isfile(arg):
+                filename = arg
+            else:
+                print "Wrong format for the argument passed on option "+opt
+        if opt in ('-l', '--list'):
+            opts_check = True
+            parse_list()
+        elif opt in ('-m', '--maintainer'):
+            opts_check = True
+            checkM  = True
+        elif opt in ('-c', '--committers'):
+            opts_check = True
+            checkC = True
+        if not opts_check:
+            assert False, "unhandled option"
+    if not file_status:
+        print "Please specify commit filename.\n"
+        usage()
+        sys.exit()
 
 def usage():
     print "\nGit commit script usage available"
-    print "Usage: \n "+ sys.argv[0] +" [options] filename"
-    print "Take the git obtained commit defined in 'filename' and send an email to selected peers."
+    print "Usage: \n "+ sys.argv[0] +" [options]"
+    print "Take the git obtained commit and send an email to selected peers. If no filename is offered then the standard 000* file is used."
     print "\nMandatory arguments to long options are mandatory for short options too."
     print "\t-m, --maintainer       send email to maintainers of the package"
     print "\t-c, --committers       send email to committers on the package"
     print "\t-l, --list             send email to a list of committers read from config file"
+    print "\t-f, --filename         selected commit file"
     print "\t-h, --help             display this help and exit"
     print "\t-v, --version          output version information and exit"
     print "\nReport "+ sys.argv[0] +" bugs to vaduva.jan.alexandry@gmail.com"
 
+def read_list():
+    with open('input/list.txt') as fp:
+        print fp.read()
+
+def parse_list():
+    with open('input/list.txt') as fp:
+        for line in fp:
+            line = "".join(line.split())
+            if line.split('-')[0] in ['e', 'enable', 'enabled']:
+                email_list.append(line.split('-')[1])
+                names_list.append(line.split('-')[2])
+
+def special_match(strg, search=re.compile(r'[^a-z0-9@.]').search):
+    return not bool(search(strg))
+
+#def check_options (opt, arg):
+#    if opt in ('-m', '--maintainer') or opt in ('-c', '--committers'):
+#        return special_match(arg)
+#    else:
+#        return os.path.isfile(arg)
+
 if __name__ == "__main__":
-	inspect_args()
+    inspect_args()
+    print filename +":"+ str(checkM)+":"+str(checkC)
+    repo = git.Repo('./')
+    print repo.git.status()
