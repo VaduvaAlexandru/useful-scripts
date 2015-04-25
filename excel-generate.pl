c2f3a062c6a7d2b5fe127cd5f2bf44eb41b1cd23
Alexandru.Vaduva <vaduva.jan.alexandru@gmail.com>
2015-04-26 02:44:09 +0300

    Updated the Excel parsing script to a more complete form.
    Signed-off-by: Alexandru.Vaduva <vaduva.jan.alexandru@gmail.com>

--- excel-generate.pl
+++ excel-generate.pl
@@ -2,8 +2,8 @@
 
 use strict;
 use warnings;
-use utf8;
-use Text::Iconv;
+use diagnostics;
+use open ':std', ':encoding(UTF-8)';
 use Spreadsheet::ParseExcel;
 use Spreadsheet::ParseXLSX;
 
@@ -22,21 +22,33 @@ if ( !defined $workbook ) {
     }
 }
 
-print "Sheets = ", $workbook->worksheet_count(), "\n";
-
 for my $worksheet ( $workbook->worksheets() ) {
     my ( $row_min, $row_max ) = $worksheet->row_range();
     my ( $col_min, $col_max ) = $worksheet->col_range();
+    my $line = 0;
 
     for my $row ( $row_min .. $row_max ) {
+        if (($line < $row) and ($line != 0) ) {
+            last;
+        }
+
         for my $col ( $col_min .. $col_max ) {
             my $cell = $worksheet->get_cell( $row, $col );
             next unless $cell;
 
-            print "Row, Col    = ($row, $col)\n";
-            print "Value       = ", $cell->value(),       "\n";
-            print "Unformatted = ", $cell->unformatted(), "\n";
-            print "\n";
+            if ( $cell->value ) {
+                print " | ", $cell->value(),       " | ";
+
+                # Do this because the "Requirements" sheet has multiple empty rows
+                if ( $cell->value eq "PMS.5.3"  ) {
+                    $line = $row;
+                }
+            }
+            else {
+                last;
+            }
         }
+        print "\n";
     }
+    print "\n";
 }
