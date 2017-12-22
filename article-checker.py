67fb2d46922b759e5688fbe445cd5f65a3ea3041
Alexandru.Vaduva <alexandru.vaduva@linux.com>
2017-12-23 00:25:17 +0200

    article-checker: update support with issue/solution words
    Parse the text line by line and word by word and try to identify
    the inapropriate syntax in order to suggest correction.
    
    Signed-off-by: Alexandru.Vaduva <alexandru.vaduva@linux.com>

--- article-checker.py
+++ article-checker.py
@@ -1,4 +1,4 @@
-#import sys
+import io
 import textract
 
 from nltk.tokenize import word_tokenize
@@ -6,19 +6,48 @@ from nltk.corpus import stopwords
 
 text = textract.process('rezumat-hids-framework.doc', extension='doc')
 
-#reload(sys)
-#sys.setdefaultencoding('utf8')
+char_encoding = "utf-8"
+with io.open("review-text.log", 'w', encoding=char_encoding) as text_file:
+    text_file.write(text.decode("unicode-escape"))
+text_file.close()
 
-tokens = word_tokenize(text.decode('utf-8'))
+#tokens = word_tokenize(text.decode('utf-8'))
 punctuation = ['(',')',';',':','[',']',',']
 stop_words = stopwords.words('english')
+#keywords = [word for word in tokens if not word in punctuation]
 
-keywords = [word for word in tokens if not word in stop_words and not word in punctuation]
+token_lists = []
+with io.open("review-text.log", encoding=char_encoding) as f:
+#with open("text.log") as f:
+    lines = f.readlines()
 
-# TODO: maybe 'stop_words' can remain in 'keywords' list
+for line in lines:
+    token_array = []
+
+    if line.strip():
+        for token in line.split():
+            if token.strip():
+                token_array.append(token)
+    if token_array:
+        token_lists.append(token_array)
+
+issues = ['study', 'investigate', 'system', 'pipeline', 'features', 'combine', 'modify', 'expand']
+solutions = ['develop', 'propose', 'model', 'representations']
+## TODO: maybe 'stop_words' can remain in 'keywords' list
 ## Identify the incorect syntaxes and eventually correct them.
+for idx, row in enumerate(token_lists):
+    for column in row:
+        if column in issues:
+            print "From line %5d remove: %s" % (idx, column)
+print "\tUse instead:\n%s" % solutions
+
+## TODO: print romanian chars correctly, at the moment they do not
+##       seem correctly encoded/decoded from 'text' variable
 
 #print text
 #print tokens
-print stop_words
+#print stop_words
 #print keywords
+
+#print token_lists
+
