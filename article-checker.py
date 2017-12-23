f8f8ea1c8629f1ea018800459169b26f206caf46
Alexandru.Vaduva <alexandru.vaduva@linux.com>
2017-12-23 21:12:31 +0200

    article-checker: add spell-checker functionality
    Signed-off-by: Alexandru.Vaduva <alexandru.vaduva@linux.com>

--- article-checker.py
+++ article-checker.py
@@ -1,10 +1,12 @@
-import io
+import io, re
 import textract
+import enchant
 
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
 text = textract.process('rezumat-hids-framework.doc', extension='doc')
+d = enchant.Dict("en_US")
 
 char_encoding = "utf-8"
 with io.open("review-text.log", 'w', encoding=char_encoding) as text_file:
@@ -31,23 +33,28 @@ for line in lines:
     if token_array:
         token_lists.append(token_array)
 
-issues = ['study', 'investigate', 'system', 'pipeline', 'features', 'combine', 'modify', 'expand']
-solutions = ['develop', 'propose', 'model', 'representations']
-## TODO: maybe 'stop_words' can remain in 'keywords' list
-## Identify the incorect syntaxes and eventually correct them.
+issues = ['study', 'investigate', 'system', 'pipeline', 'features', 'combine', \
+          'modify', 'expand', 'etc', 'for various reasons', 'this', 'that', 'it', \
+          'these', 'which']
+solutions = ['develop', 'propose', 'model', 'representations', \
+             '"That" is defining; "which" is nondefining']
+
 for idx, row in enumerate(token_lists):
     for column in row:
         if column in issues:
             print "From line %5d remove: %s" % (idx, column)
+        if d.check(column) is False and re.match('^[a-zA-Z ]*$', column):
+            print "From line %5d please correct: %s" % (idx, column)
 print "\tUse instead:\n%s" % solutions
 
 ## TODO: print romanian chars correctly, at the moment they do not
-##       seem correctly encoded/decoded from 'text' variable
+##       seem correctly encoded/decoded from 'text' variable -> POSTPONED
 
-#print text
-#print tokens
-#print stop_words
-#print keywords
+## TODO: identify acronyms and see if they were defined the first time
+##       they were used, else signal this as an issue
 
-#print token_lists
+## TODO: identify if the paper has the proper structure, for this
+##       NLP an ML might need to be used
 
+## TODO: maybe 'stop_words' can remain in 'keywords' list
+## Identify the incorect syntaxes and eventually correct them. -> DONE
